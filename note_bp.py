7f413d14ad8cbb4a61f7767d46dcc6ea53318c23
TatyanaFilimonova <81884634+TatyanaFilimonova@users.noreply.github.com>
2021-11-07 00:20:08 +0200

    Add files via upload
    1. Patched edit Note and add note features (add_note.html, edit_note_save.html). Now, when new tag is created, the changes in text field doesnt disapiered .Used java script to store data in sessionStorage.
    
    2. Patched find notes functionality: used outerjoin between Note, Tag and TagsAndNotes tables to make serch in many-to-many relation data set (notes_data_classes.py)
    
    3. Patched note blueprint to add the highlights for keywords in the text. Now all keywords in search results highlighted with light-blue color (note_bp.py)
    
    4. From users_data_classes.py deleted mongo_db functions.

---
+++ note_bp.py
@@ -0,0 +1,228 @@
+"""
+Blueprint Note for Flask app Bot
+handlers responsible for operations with Notebook
+"""
+
+# pylint: disable=W0614
+# pylint: disable=W0401
+# pylint: disable=E0402
+# pylint: disable=R0801
+
+# local packages
+import os
+import sys
+import re
+from flask import Blueprint, render_template, request, redirect
+from markupsafe import Markup
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+if __package__ == "" or __package__ is None:
+    import global_var
+    from contact_bp import *
+else:
+    from . import global_var
+    from .contact_bp import *
+
+
+note_bp = Blueprint("note", __name__, url_prefix="/note")
+
+
+@note_bp.route("/find_notes", methods=["GET", "POST"])
+def find_notes():
+    """
+    Search for notes in Notebook
+    GET - prepare search form
+    POST - make search by keywords from web form and represent the results.
+    Search through Text and Keywords fields
+    :return: rendered HTML
+    """
+    results = []
+    if request.method == "POST":
+        data = request.form.to_dict(flat=False)
+        if "Keywords" not in data:
+            abort(400, "Wrong request data")
+        keywords = clean_search_str(request.form.get("Keywords"))
+        for k in [kw.strip() for kw in keywords.split(",")]:
+            for res in global_var.note_book.get_notes(session['user_id'], k):
+                if isinstance(res, str):
+                    return html_error(res)
+                if res not in results:
+                    regex = fr"(?P<to_subst>{k}[\w]*[\ ]?[$]?)"
+                    subst = r"<mark style='background-color: #34d8eb;'>\g<to_subst></mark>"
+                    res.text = Markup(re.sub(regex, subst, res.text, 0, re.MULTILINE | re.I))
+                    results.append(res)
+
+        return render_template("note/find_notes_found.html", result=results)
+    tags = global_var.note_book.get_all_tags(session['user_id'])
+    return render_template("note/find_notes_search.html", tags=tags)
+
+
+@note_bp.route("/find_notes/<tag>/<target>", methods=["GET", "POST"])
+def find_notes_by_tag(tag, target):
+    results = global_var.note_book.get_notes(session['user_id'], tag)
+    if isinstance(results, str):
+        return html_error(results)
+    for i in range(0, len(results)):
+        regex = fr"(?P<to_subst>{tag}[\w]*[\ ]?[$]?)"
+        subst = r"<mark style='background-color: #34d8eb;'>\g<to_subst></mark>"
+        results[i].text = Markup(re.sub(regex, subst, results[i].text, 0, re.MULTILINE | re.I))
+    if target in ['find', 'edit']:
+        return render_template("note/find_notes_found.html", result=results)
+    elif target in ['delete']:
+        return render_template("note/delete_notes_found.html", result=results)
+
+@note_bp.route("/add_tag", methods=["GET", "POST"])
+def create_tag():
+    print("TRY TO CREATE TAG - START")
+    res = global_var.note_book.create_tag(session['user_id'],
+                                          session['current_note'],
+                                          request.form.get("new_tag")
+                                          )
+    if isinstance(res, str):
+        return html_error(res)
+    if session['current_note']:
+        return redirect(f"save_note/{session['current_note']}")
+    else:
+        return redirect("add_note")
+
+
+
+@note_bp.route("/show_all_notes", methods=["GET"])
+def show_all_notes():
+    """
+    Show all the Notes from Notebook
+    :return: rendered HTML
+    """
+    result = global_var.note_book.get_all_notes(session['user_id'])
+    if isinstance(result, str):
+        return html_error(result)
+    return render_template("note/all_notes.html", result=result)
+
+
+@note_bp.route("/add_note", methods=["GET", "POST"])
+def add_note():
+    """
+    Add new note to DB
+    GET - prepare the web form for user to input Note data - Text and keywords
+    POST - saving data from web form and save Note to DB
+    :return:
+    """
+    if request.method == "POST":
+        data = request.form.to_dict(flat=False)
+        if "Text" not in data:
+            abort(400, "Wrong request data")
+        text = request.form.get("Text")
+        if len(text) > 500:
+            text = text[0:500]
+        res = global_var.note_book.insert_note(
+            session['user_id'],
+            request.form.getlist("Tags"),
+            text
+        )
+        if res == 0:
+            return render_template("note/add_note_OK.html")
+        return html_error(res)
+    session['current_note'] = 0
+    tags = global_var.note_book.get_all_tags(session['user_id'])
+    return render_template("note/add_note.html", tags=tags)
+
+
+@note_bp.route("/edit_note", methods=["GET", "POST"])
+def edit_note():
+    """
+    Prepare the list of the Note that user would like to update
+    GET - prepare the we form for user input
+    POST - search notes using user input and represent the results
+    :return: rendered HTML
+    """
+    results = []
+    if request.method == "POST":
+        data = request.form.to_dict(flat=False)
+        if "Keywords" not in data:
+            abort(400, "Wrong request data")
+        keywords = clean_search_str(request.form.get("Keywords"))
+        for k in [kw.strip() for kw in keywords.split(",")]:
+            for res in global_var.note_book.get_notes(session['user_id'], k):
+                if res not in results:
+                    results.append(res)
+        return render_template("note/find_notes_found.html", result=results)
+    tags = global_var.note_book.get_all_tags(session['user_id'])
+    return render_template("note/find_notes_search.html", tags=tags)
+
+
+@note_bp.route("/save_note/<note_id>", methods=["GET", "POST"])
+def save_note(note_id):
+    """
+    Save updated Note by note_id with data from web form dict {'Keywords':, 'Text':}
+    GET - prepare web form to edit Note selected by ID
+    POST - update note selected by ID with web form data
+    :param note_id: int
+    :return: rendered HTML
+    """
+    if request.method == "POST":
+        data = request.form.to_dict(flat=False)
+        if "Tags" not in data or "Text" not in data:
+            abort(400, "Wrong request data")
+        text = request.form.get("Text")
+        if len(text) > 500:
+            text = text[0:500]
+        res = global_var.note_book.update_note(
+            note_id, request.form.getlist("Tags"), text)
+        if res == 0:
+            return render_template("note/edit_notes_OK.html")
+        return html_error(res)
+    tags = global_var.note_book.get_all_tags(session['user_id'])
+    result = global_var.note_book.get_note_by_id(note_id)
+    session['current_note'] = note_id
+    return render_template("note/edit_notes_save.html", res=result, tags=tags)
+
+
+@note_bp.route("/delete_note", methods=["GET", "POST"])
+def delete_note():
+    """
+    Make a preparations to delete a Note from DB
+    GET - create a from to search a Note which user would like to delete
+    POST - search Note by user put in Keywords and do the representation of search resuls.
+    Search made by Note and keywords fields
+    :return: rendered HTML
+    """
+    results = []
+    if request.method == "POST":
+        data = request.form.to_dict(flat=False)
+        if "Keywords" not in data:
+            abort(400, "Wrong request data")
+        keywords = clean_search_str(request.form.get("Keywords"))
+        for k in [kw.strip() for kw in keywords.split(",")]:
+            for res in global_var.note_book.get_notes(session['user_id'], k):
+                if isinstance(res, str):
+                    return html_error(res)
+                if res not in results:
+                    results.append(res)
+        return render_template("note/delete_notes_found.html", result=results)
+    tags = global_var.note_book.get_all_tags(session['user_id'])
+    return render_template("note/delete_notes_search.html", tags=tags)
+
+
+@note_bp.route("/delete_note/<note_id>", methods=["GET", "POST"])
+def note_delete_(note_id):
+    """
+    Delete Note from notebook by given note_id
+    :param note_id:
+    :return: rendered HTML
+    """
+    res = global_var.note_book.delete_note(note_id)
+    if res == 0:
+        return render_template("note/delete_notes_OK.html", id=note_id)
+    return html_error(res)
+
+
+@note_bp.route("/delete_notes", methods=["POST"])
+def delete_notes():
+    note_ids = request.form.getlist("Delete")
+    for note_id in note_ids:
+        res = global_var.note_book.delete_note(note_id)
+        if res != 0:
+            return html_error(res)
+    return render_template("note/delete_notes_OK.html", id=note_ids)
---
+++ notes_data_classes.py
@@ -0,0 +1,264 @@
+"""
+Base class for Notebook entity and its realisations
+for mongo and Postgres DataBases
+"""
+
+# pylint: disable=W0614
+# pylint: disable=R0903
+# pylint: disable=W0703
+# pylint: disable=W0401
+# pylint: disable=E0402
+# pylint: disable=R0801
+
+from datetime import datetime
+from datetime import date
+import re
+import sys
+import os
+from abc import ABC, abstractmethod
+from sqlalchemy import or_, update, delete
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+if __package__ == "" or __package__ is None:
+    from SQL_alchemy_classes import *
+else:
+    from .SQL_alchemy_classes import *
+
+
+class Notebook(ABC):
+    """
+    Base class fore Notebook entity
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def get_all_notes(self, user_id):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def get_notes(self, user_id, keyword):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def get_note_by_id(self, note_id):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def update_note(self, note_id, keywords, text):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def insert_note(self, user_id, keywords, text):
+        """
+        Abstract method
+        """
+
+    @abstractmethod
+    def delete_note(self, note_id):
+        """
+        Abstract method
+        """
+
+
+
+class NotebookPSQL(Notebook):
+    """
+    Class Notebook that operates with Postgres
+    """
+
+    def __init__(self, session=None):
+        super().__init__()
+        self.session = session
+        self.notes = []
+
+    def get_all_notes(self, user_id):
+        """
+        Find all the notes in DB
+        :return: list of NotePSQL
+        """
+        self.notes = []
+        result = (
+            self.session.query(
+                Note.note_id, Note.user_id, Note.text, Note.created_at
+            ).filter(Note.user_id == user_id).order_by(Note.note_id).all()
+        )
+        for res in result:
+            tags = self.session.query(
+                Tag.tag
+            ).join(TagsAndNotes).filter(TagsAndNotes.note_id == res.note_id).all()
+            note = NotePSQL(res)
+            note.tags = [tag_.tag for tag_ in tags]
+            self.notes.append(note)
+        return self.notes
+
+    def get_notes(self, user_id, keyword):
+        """
+        Select from DB Notes find by keyword for fields text, keywords
+        :param keyword: str
+        :return: list of NotePSQL
+        """
+        self.notes = []
+        if keyword != "":
+
+            result = (
+                self.session.query(
+                    Note.note_id, Note.user_id, Note.text, Note.created_at
+                ).outerjoin(TagsAndNotes).outerjoin(Tag).filter(Note.user_id == user_id,
+                    or_(
+                        func.lower(Tag.tag).like(func.lower(f"%{keyword}%")),
+                        func.lower(Note.text).like(func.lower(f"%{keyword}%")),
+                    )
+                ).distinct()
+                .order_by(Note.note_id)
+                .all()
+            )
+            for res in result:
+                tags = self.session.query(
+                    Tag.tag
+                ).join(TagsAndNotes).filter(TagsAndNotes.note_id == res.note_id).all()
+                note = NotePSQL(res)
+                note.tags = [tag_.tag for tag_ in tags]
+                self.notes.append(note)
+            return self.notes
+        return []
+
+    def get_note_by_id(self, note_id):
+        """
+        Select from DB Note by given note_id
+        :param note_id: int
+        :return: NotePSQL
+        """
+        result = (
+            self.session.query(
+                Note.note_id, Note.user_id, Note.text, Note.created_at
+            ).filter(Note.note_id == note_id).first()
+        )
+        if result:
+            tags = self.session.query(
+                Tag.tag, Tag.tag_id
+            ).join(TagsAndNotes).filter(TagsAndNotes.note_id == result.note_id).all()
+            note = NotePSQL(result)
+            tag_lst = []
+            for tag_ in tags:
+                tag_lst.append(tag_)
+            res = NotePSQL(result)
+            res.tags = tags
+            return res
+        return None
+
+    def get_all_tags(self, user_id):
+        result = (
+            self.session.query(
+                Tag.tag, Tag.tag_id
+            ).filter(Tag.user_id == user_id).all()
+        )
+        return result
+
+    def create_tag(self, user_id, note_id, tag):
+        try:
+            if self.session.query(Tag.tag).filter(Tag.tag==tag, Tag.user_id == user_id).first() is None:
+                print("TRY TO CREATE TAG - ENTER CLASS METHOD")
+                tag = Tag(tag=tag, user_id=user_id)
+                self.session.add(tag)
+                self.session.commit()
+                print("COMMIT")
+            return 0
+        except Exception as error:
+            print(f"ERROR: {error}")
+            return str(error)
+
+
+    def update_note(self, note_id, tags, text):
+        """
+        Update Note selected by note_id with new keywords and text
+        :param note_id: int
+        :param keywords: str
+        :param text: str
+        :return: 0 if OK or error otherwise
+        """
+        try:
+            self.session.execute(
+                update(Note, values={Note.text: text}).filter(
+                    Note.note_id == note_id
+                )
+            )
+            sql_stmt = delete(TagsAndNotes).where(TagsAndNotes.note_id == note_id)
+            run_sql(self.session, sql_stmt)
+            if tags:
+                for tag in tags:
+                    tag_and_note = TagsAndNotes(note_id=note_id, tag_id=int(tag))
+                    self.session.add(tag_and_note)
+            self.session.commit()
+            return 0
+        except Exception as error:
+            self.session.rollback()
+            return error
+
+    def insert_note(self, user_id, tags, text):
+        """
+        Insert new Note to DB
+        :param keywords: str
+        :param text: str
+        :return: 0 if OK or error otherwise
+        """
+        try:
+            note = Note(user_id=user_id, created_at=date.today(), text=text)
+            self.session.add(note)
+            self.session.commit()
+            for tag in tags:
+                tag_and_note = TagsAndNotes(note_id=note.note_id, tag_id=int(tag))
+                self.session.add(tag_and_note)
+            self.session.commit()
+            return 0
+        except Exception as error:
+            self.session.rollback()
+            return error
+
+    def delete_note(self, note_id):
+        """
+        Delete Note from DB by note_id
+        :param note_id: int
+        :return: 0 if OK or exception otherwise
+        """
+        sql_stmt = delete(Note).where(Note.note_id == note_id)
+        return run_sql(self.session, sql_stmt)
+
+class NoteAbstract(ABC):
+    """
+    Abstract Note class
+    """
+
+    @abstractmethod
+    def __init__(self):
+        self.note_id = None
+        self.created_at = None
+        self.text = None
+        self.user_id = None
+
+
+class NotePSQL(NoteAbstract):
+    """
+    Class Notes initialized from SQLAlchemy query
+    """
+
+    def __init__(self, note):
+        super().__init__()
+        self.note_id = note.note_id
+        self.created_at = note.created_at
+        self.text = note.text
+        self.user_id = note.user_id
